3b635ca2096350446fd11b0d421330209c32d8f7
goolygoolybongbong <goolygoolybongbong@gmail.com>
2022-08-27 17:30:27 +0900

    initial file add

---
+++ src/main/java/com/example/eeyore/EeyoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.eeyore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeyoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeyoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eeyore/EeyoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eeyore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeyoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
